a831109f910ce95047ac67f0f9c06378748fe43f
Om Das <omdas7711@gmail.com>
2026-07-06 10:17:19 +0530

    Refactor TodoForm component for clarity

--- T0_DO/src/components/TodoForm.jsx
+++ T0_DO/src/components/TodoForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useTodo } from '../contexts/TodoContext';
 
+
 function TodoForm() {
     const [todo, setTodo] = useState("")
     const {addTodo} = useTodo()
